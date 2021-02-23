30bfdba86000e1468633861179234884d8f54158
oh.chi.hooooon <oh.chi.hooooon@gmail.com>
2021-02-23 09:28:57 +0900

    update file

--- document/hadoop-spark/reducer.py
+++ document/hadoop-spark/reducer.py
@@ -23,5 +23,6 @@ for line in sys.stdin:
             print ('%s\t%s' % (current_word, current_count)) 
         current_count = count 
         current_word = word 
+        
 if current_word == word: 
     print ('%s\t%s' % (current_word, current_count))
