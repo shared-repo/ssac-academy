897956130b8988b10c096f8f23f6e031dd51f7e5
oh.chi.hooooon <oh.chi.hooooon@gmail.com>
2021-03-08 13:21:39 +0900

    update file

--- workspace/code-workspace/ml/content_based_movie_recommender.py
+++ workspace/code-workspace/ml/content_based_movie_recommender.py
@@ -10,4 +10,33 @@ class ContentBasedMovieRecommender:
         weighted_movie = pd.read_csv('data-files/weighted_movie.csv')
         self.weighted_movie = weighted_movie
 
-        pass
+    def find_sim_movie(self, title, top_n=10):
+        title_movie = self.weighted_movie[self.weighted_movie['title'] == title]
+        title_index = title_movie.index.values
+        
+        # top_n의 2배에 해당하는 쟝르 유사성이 높은 index 추출 
+        similar_indexes = self.genre_sim_sorted_ind[title_index, :(top_n*2)]
+        similar_indexes = similar_indexes.reshape(-1)
+
+        # 기준 영화 index는 제외
+        similar_indexes = similar_indexes[similar_indexes != title_index]
+        
+        # top_n의 2배에 해당하는 후보군에서 weighted_vote 높은 순으로 top_n 만큼 추출 
+        return self.weighted_movie.iloc[similar_indexes].sort_values('weighted_vote', ascending=False)[:top_n]
+
+if __name__ == "__main__": # python content_based_movie_recommender.py 형식으로 실행한 경우 ( import content_based_movie_recommender 로 실행한 경우에는 실행되지 않는 영역)
+
+    recommender = ContentBasedMovieRecommender()
+
+    while True:
+        title = input("Input Movie Title (Enter for Quit) : ")
+        
+        if title is None or len(title) == 0:
+            break
+
+        try:
+            top_n_movies = recommender.find_sim_movie(title)
+            print("[ RECOMMENDED MOVIES ]")
+            print(top_n_movies[['title', 'vote_average', 'weighted_vote']])
+        except:
+            print("Fail to Recommend !!!!")
